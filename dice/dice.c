77875a776353adb0323d5b2677ad1bae050734ba
James Kim <yameskim@gmail.com>
2018-04-05 01:37:37 -0400

    emcc dice

--- dice/dice.c
+++ dice/dice.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <emscripten/emscripten.h>
 
 int main(void)
 {
-  printf("%s\n", "WebAssembly is the future!");
+  // srand(time(NULL)) initializes the PRNG (Pseudo-Random Number Generator)
+  // (recommended to be done in the main when app starts.)
+  // Seed value generator:
+  srand(time(NULL));
+  printf("%s\n", "Loaded WebAssembly, the future is here!");
+}
+
+int EMSCRIPTEN_KEEPALIVE rolla_dice()
+{
+  return rand() % 6 + 1;
 }
