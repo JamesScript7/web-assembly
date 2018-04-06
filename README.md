6ca0edf7882e5a9599ff3d693084673f961d09f5
James Kim <yameskim@gmail.com>
2018-04-06 14:33:15 -0400

    Add emscripten locally

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Some exercises with WebAssembly in C lang.
 
 Helpful sources:
 
-https://developer.mozilla.org/en-US/docs/WebAssembly/C_to_wasm
-
 https://www.dovico.com/blog/2018/01/22/an-introduction-to-webassembly/
 
 Hello world:
 
 http://webassembly.org/getting-started/developers-guide/
+
+https://developer.mozilla.org/en-US/docs/WebAssembly/C_to_wasm
 
 **SKILLS:** HTML, C, and emscripten.
 
--- hello-world/hello2/hello2.c
+++ hello-world/hello2/hello2.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include "../emscripten/emscripten.h"
 
-int main(int argc, char ** argv)
+int main()
 {
-	// This gets converted to console.log!
 	printf("Hello, world!\n");
 	return 0;
 }
+
+void EMSCRIPTEN_KEEPALIVE myFunction(int val)
+{
+	printf("myFunction in C: with val %i, passed in.\n", val);
+}
+
+/*
+TERMINAL:
+emcc hello2.c -s WASM=1 -o hello2.html --shell-file ../html_template/shell_minimal.html -s NO_EXIT_RUNTIME=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall"]'
+
+HTML:
+<!-- START: Pass value into C function! -->
+<input type="text" id="txtValue">
+<input type="button" value="Pass Value" onclick="PassValue();">
+<!-- END: Pass value into C function! -->
+
+SCRIPT:
+function PassValue(){
+	Module.ccall(
+		'myFunction', // Name of C function
+		null,         // Return type
+		['number'],   // Argument types
+		[ parseInt(document.getElementById("txtValue").value,10) ]
+	);
+}
+*/
