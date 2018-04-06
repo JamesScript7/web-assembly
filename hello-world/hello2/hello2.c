08dde906dced242a8ac8c936cbec1665f58b2d85
James Kim <yameskim@gmail.com>
2018-04-06 15:11:17 -0400

    Add cwrap method

--- hello-world/hello2/hello2.c
+++ hello-world/hello2/hello2.c
@@ -13,8 +13,14 @@ void EMSCRIPTEN_KEEPALIVE myFunction(int val)
 }
 
 /*
+
 TERMINAL:
-emcc hello2.c -s WASM=1 -o hello2.html --shell-file ../html_template/shell_minimal.html -s NO_EXIT_RUNTIME=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall"]'
+emcc hello2.c -s WASM=1 -o hello2.html --shell-file ../html_template/shell_minimal.html -s NO_EXIT_RUNTIME=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]'
+
+--shell-file ../html_template/shell_minimal.html
+-s NO_EXIT_RUNTIME=1
+// In order to use Module.ccall and/or Module.cwrap
+-s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]'
 
 HTML:
 <!-- START: Pass value into C function! -->
@@ -24,11 +30,20 @@ HTML:
 
 SCRIPT:
 function PassValue(){
+	//ccall method:
 	Module.ccall(
 		'myFunction', // Name of C function
 		null,         // Return type
 		['number'],   // Argument types
 		[ parseInt(document.getElementById("txtValue").value,10) ]
 	);
+
+	// cwrap method:
+	let cwrapMethod = Module.cwrap(
+		'myFunction',
+		null,
+		['number']
+	);
+	cwrapMethod(10);
 }
 */
